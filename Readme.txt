d3dd976445f7f0a7ad5b8863a56bb598c518319a
FRC Team 293 Hopewell, NJ <293.First@gmail.com>
2017-02-01 16:35:42 -0500

    Readme Numbers

--- Readme.txt
+++ Readme.txt
@@ -1,19 +1,19 @@
 Main Stripes
 Idle Modes:
-	Rainbow
+	Rainbow  n/a #
 Autonomous: (Depending on Team we’re on)
-	Blue Chasing  
-	Red Chasing
+	Blue Chasing  255
+	Red Chasing	254
 Shooting:
-	Orange Chasing when shooting
-	Solid Orange when Feeding
-	Red when low Goal thing is up
+	Orange Chasing when shooting	253
+	Solid Orange when Feeding	252
+	Red when low Goal thing is up	251
 Gear:
-	Yellow when get a gear (stay for 5 seconds)
-	Green when lined up
+	Yellow when get a gear (stay for 5 seconds)250	
+	Green when lined up	249
 Side Stripes:
-White when driving forward.
-	Purple when driving backward.
+	White when driving forward. 0
+	Purple when driving backward.  1
 
 15s left
-	Blue Flashing….?
+	Blue Flashing….?  2
--- _2017ArduinoLEDCode/_2017ArduinoLEDCode.ino
+++ _2017ArduinoLEDCode/_2017ArduinoLEDCode.ino
@@ -1,128 +1,122 @@
-#include <TimerOne.h>   // Timer 1 is also used by the strip to send pixel clocks
+#include <TimerOne.h>
 #include "LPD6803.h"
 
+//Example to control LPD6803-based RGB LED Modules in a strand
+// Original code by Bliptronics.com Ben Moyes 2009
+//Use this as you wish, but please give credit, or at least buy some of my LEDs!
+
+// Code cleaned up and Object-ified by ladyada, should be a bit easier to use
+#define MAX_BRIGHTNESS 1.0    //float from 0 to 1
+#define LED_MODULES 6        //number of chips on led strip
 /*****************************************************************************/
+int allLeds[LED_MODULES];
+// Choose which 2 pins you will use for output.
+// Can be any valid output pins.
+int dataPin = 2;       // 'yellow' wire
+int clockPin = 3;      // 'green' wire
+// Don't forget to connect 'blue' to ground and 'red' to +5V
 
-int dataPin = 3;       // 'yellow' wire
-int clockPin = 2;      // 'green' wire
-int pixels = 5;       //number of pixels
+// Timer 1 is also used by the strip to send pixel clocks
 
-LPD6803 strip = LPD6803(pixels, dataPin, clockPin);   //defining the LED strip: number of pixels, dataPin, clockPin
-int state=0;   
-int parse=0;
-int mainLedStatus=0;
-int sideLedStatus=0;
+// Set the first variable to the NUMBER of pixels. 20 = 20 pixels in a row
+LPD6803 strip = LPD6803(50, dataPin, clockPin);
+
 
 void setup() {
-  // The Arduino needs to clock out the data to the pixels
-  // this happens in interrupt timer 1, we can change how often
-  // to call the interrupt. setting CPUmax to 100 will take nearly all all the
-  // time to do the pixel updates and a nicer/faster display, 
-  // especially with strands of over 100 dots.
-  // (Note that the max is 'pessimistic', its probably 10% or 20% less in reality)
   
   strip.setCPUmax(50);  // start with 50% CPU usage. up this if the strand flickers or is slow
-  strip.begin();        // Start up the LED counter 
-  strip.show();         // Update the strip, to start they are all 'off'
-
-  Serial.begin(115200);
+  strip.begin();
+  strip.show();
 }
 
 
-void loop(){                                                      // perform different functions when the robot is doing different things\
- rainbow(5, 0, pixels);   
-  if(state<99){
-    mainLedStatus=state;
-  }
-  if(state>99){
-    sideLedStatus=state-99;
-  }
- /*switch (mainLedStatus) {
-    //main stripes:
-    case 1: rainbow(50, 1, pixels);                     //idle: rainbow- all pixels
-    break;
-    case 2: colorChase(Color(255,0,0), Color(0,0,0), 50, 1, pixels);  //blue/red chasing (if statement)
-    break;
-    case 3: colorChase(Color(255, 165, 0), Color(0,0,0), 50, 1, pixels);  //shooting: orange chasing
-    break;
-    case 4: colorWipe(Color(255, 165, 0), 50, 1, pixels);  // feeding: solid orange 
-    break;
-    case 5: colorWipe(Color(255, 0, 0), 50, 1, pixels);  // low goal: solid red
-    break;
-    case 6: {
-      colorWipe(Color(255, 255, 0), 50, 1, pixels); // holding a gear: yellow for 5s
-      delay(5000);  
-    }
-    break;
-    case 7: colorWipe(Color(0, 255, 0), 50, 1, pixels);  // lined up for gear: green
-    break;
-  }
-  switch(sideLedStatus){
-     //side stripes:
-    case 1: colorWipe(Color(0, 0, 0), 50, 1, pixels);  //driving forward: white
-    break;
-    case 2: colorWipe(Color(255, 0, 255), 50, 1, pixels);  //driving backwards:  purple
-    break;
-  }*/
-}
-
-void colorWipe(uint16_t c, uint8_t wait, int a, int b) { //changing all pixels from a to b to color 'c'
-  int i;
-    for (i=(a-1); i < (b+1); i++) {                      //pixel numbers begin with 0
-        strip.setPixelColor(i, c);
-        strip.show();
-        delay(wait);
-    }
-}
-
-void colorChase(uint16_t c, uint16_t bc, uint8_t wait, int a, int b) { //changing all pixels from a to b to color 'c'
-  int i;
-    for (i=(a-1); i < (b+1); i++) {                      
-        strip.setPixelColor(i, c);
-        strip.setPixelColor((i-1), bc);
-        strip.show();
-        delay(wait);
-    }
-}
-
-void rainbow(uint8_t wait, int a, int b) {              // wait, starting pixel, ending pixel
-  int i, j;
-   
-  for (j=0; j < 96 * 3; j++) {                          // 3 cycles of all 96 colors in the wheel
-    for (i=(a-1); i < (b+1); i++) {                     // create rainbow on all pixels from a to b
-      strip.setPixelColor(i, Wheel( (i + j) % 96));
-    }  
-    strip.show();                                       // write all the pixels out
-    delay(wait);
-  }
-}
-
-// makes the rainbow wheel equally distributed along the chain
-
-void rainbowCycle(uint8_t wait, int a, int b) { //wait, starting pixel, ending pixel
-  int i, j;
+void loop() {
+  // Some example procedures showing how to display to the pixels
   
-  for (j=0; j < 96 * 5; j++) {                                                       // 5 cycles of all 96 colors in the wheel
-    for (i=a; i < b+1; i++) {
-        strip.setPixelColor(i, Wheel( ((i * 96 / strip.numPixels()) + j) % 96) );    // use each pixel as a fraction of the full 96-color wheel: i / strip.numPixels()                                                                           
-    }                                                                                // add in j which makes the colors go around per pixel
-    strip.show();   // write all the pixels out                                      // the % 96 is to make the wheel cycle around 
-    delay(wait);
+//  colorWipe(Color(63, 0, 0), 50);
+  //colorWipe(Color(0, 63, 0), 50);
+  //colorWipe(Color(0, 0, 63), 50);
+
+    purple(1.0,allLeds,6);
+    strip.show();
+}
+int j = 0;
+unsigned int preRainbowR[LED_MODULES];
+unsigned int preRainbowG[LED_MODULES];
+unsigned int preRainbowB[LED_MODULES];
+
+void createRainbow(int type){
+  if(type == 0){
+    for(int i = 0;i < LED_MODULES;i++){
+      Wheel1(MAX_BRIGHTNESS,i % 96,i);
+    }
+  }else{
+    for(int i = 0;i < LED_MODULES;i++){
+      Wheel1(MAX_BRIGHTNESS,(i * 96 / LED_MODULES) % 96,i);
+    }
   }
 }
 
-/* Helper functions */
+void resetRainbow(){
+  j = 0;
+}
 
-// Create a 15 bit color value from R,G,B
-unsigned int Color(byte r, byte g, byte b)
-{
-  //Take the lowest 5 bits of each value and append them end to end
-  return( ((unsigned int)r & 0x1F )<<10 | ((unsigned int)g & 0x1F)<<5 | (unsigned int)b & 0x1F);
+void setToColor(uint32_t color, int* pins,int numPins){
+  for (int i=0; i<numPins; i++) {
+    strip.setPixelColor(pins[i], color);
+  }
+  strip.show();
+}
+
+void red(double brightness,int* pins,int numPins){            //turns all leds red, brightness 0.0-1.0
+  setToColor(Color(0,0,(int)(brightness*31.0)),pins,numPins);
+}
+
+void green(double brightness,int* pins,int numPins){            //turns all leds green, brightness 0.0-1.0
+  setToColor(Color(0,(int)(brightness*31.0),0),pins,numPins);
+}
+
+void blue(double brightness,int* pins,int numPins){            //turns all leds blue, brightness 0.0-1.0
+  setToColor(Color((int)(brightness*31.0),0,0),pins,numPins);
+}
+
+void yellow(double brightness,int* pins,int numPins){            //turns all leds yellow, brightness 0.0-1.0
+  setToColor(Color(0,(int)(brightness*20.0),(int)(brightness*31.0)),pins,numPins);
+}
+
+void purple(double brightness,int* pins,int numPins){            //turns all leds yellow, brightness 0.0-1.0
+  setToColor(Color((int)(brightness*31.0),0,(int)(brightness*31.0)),pins,numPins);
+}
+
+void turnOff(int* pins,int numPins){
+  for (int i=0; i<numPins; i++) {
+    strip.setPixelColor(pins[i], Color(0,0,0));
+  }
+  strip.show();
+}
+
+unsigned int Color(byte r, byte g, byte b) {
+ return( ((unsigned int)g & 0x1F )<<10 | ((unsigned int)b & 0x1F)<<5 | (unsigned int)r & 0x1F);
+}
+
+void rainbow(double brightness,int* pins,int numPins,bool usePremadeRainbow) {
+  if(j == 96*3){
+    j = 0;
+  }
+    for (int i=0; i < numPins; i++) {
+      if(usePremadeRainbow){
+        strip.setPixelColor(pins[i], Color(brightness*preRainbowR[i],brightness*preRainbowG[i],brightness*preRainbowB[i]));
+      }else{
+        strip.setPixelColor(pins[i], Wheel(brightness, (i + j) % 96));
+      }
+    }  
+    strip.show();   // write all the pixels out
+    j++;
 }
 
 //Input a value 0 to 127 to get a color value.
 //The colours are a transition r - g -b - back to r
-unsigned int Wheel(byte WheelPos)
+unsigned int Wheel(double brightness,byte WheelPos)
 {
   byte r,g,b;
   switch(WheelPos >> 5)
@@ -143,8 +137,59 @@ unsigned int Wheel(byte WheelPos)
       g=0;                  //green off
       break; 
   }
+  r *= brightness;
+  g *= brightness;
+  b *= brightness;
   return(Color(r,g,b));
 }
 
-    
-    
+void Wheel1(double brightness,byte WheelPos,int pin)
+{
+  byte r,g,b;
+  switch(WheelPos >> 5)
+  {
+    case 0:
+      r=31- WheelPos % 32;   //Red down
+      g=WheelPos % 32;      // Green up
+      b=0;                  //blue off
+      break; 
+    case 1:
+      g=31- WheelPos % 32;  //green down
+      b=WheelPos % 32;      //blue up
+      r=0;                  //red off
+      break; 
+    case 2:
+      b=31- WheelPos % 32;  //blue down 
+      r=WheelPos % 32;      //red up
+      g=0;                  //green off
+      break; 
+  }
+  r *= brightness;
+  g *= brightness;
+  b *= brightness;
+  preRainbowR[pin] = r;
+  preRainbowG[pin] = g;
+  preRainbowB[pin] = b;
+}
+
+// Slightly different, this one makes the rainbow wheel equally distributed 
+// along the chain
+void rainbowCycle(double brightness,int* pins,int numPins,bool usePremadeRainbow) {
+  if(j == 96*5){
+    j = 0;
+  }
+  
+   for (int i=0; i < numPins; i++) {
+      // tricky math! we use each pixel as a fraction of the full 96-color wheel
+      // (thats the i / numPins part)
+      // Then add in j which makes the colors go around per pixel
+      // the % 96 is to make the wheel cycle around
+      if(usePremadeRainbow){
+        strip.setPixelColor(i, Color(brightness*preRainbowR[i],brightness*preRainbowG[i],brightness*preRainbowB[i]));
+      }else{
+        strip.setPixelColor(i, Wheel(brightness, ((i * 96 / numPins) + j) % 96) );
+      }
+    }  
+    strip.show();   // write all the pixels out
+    j++;
+}
